aaa4616183a0266108ca4b7ccb053b2dd6e13477
Brenson <brensonw300@gmail.com>
2020-11-12 08:49:46 -0800

    Added migrations folder with a file with code inside it to create table

---
+++ data/migrations/20201112223044_cars.js
@@ -0,0 +1,15 @@
+
+
+exports.up = function (knex) {
+    return knex.schema.createTable('cars-info', (tbl) => {
+        tbl.increments();
+
+        tbl.toString('make', 255).notNullable();
+        tbl.string('model', 255).notNullable();
+        tbl.integer('year').notNullable();
+    });
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('cars-info');
+};
