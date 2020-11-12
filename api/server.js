2d576c5f31045496ce1dc096ee48f92dc84ff9d6
Brenson <brensonw300@gmail.com>
2020-11-12 08:34:41 -0800

    Added server js file inside api folder with all code CRUD operations

---
+++ api/server.js
@@ -0,0 +1,59 @@
+const { response } = require('express');
+const express = require('express');
+const carsModel = require('../api/cars/carsModel');
+const Cars = require('../api/cars/carsModel');
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'up'});
+});
+
+server.get('/all', (req, res) => {
+    Cars.getAll()
+    .then((response) => {
+        res.status(200).json(response);
+    })
+    .catch((err) => {
+        res.status(500).json(err);
+    });
+});
+
+server.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    Cars.remove(id)
+    .then((response) => {
+        res.status(200).json(response);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+server.post('/', (req, res) => {
+    Cars.insert(req.body)
+    .then((response) => {
+        res.status(201).json(response);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+server.put("/:id", (req, res) => {
+    const { id } = req.params;
+  
+    Cars.update(id, req.body)
+      .then((response) => {
+        res.status(200).json(response);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(error);
+      });
+  });
+  
+  module.exports = server;
